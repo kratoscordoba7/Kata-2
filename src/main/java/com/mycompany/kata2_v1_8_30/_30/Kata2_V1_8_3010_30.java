75574eeec464d87a4052a0a7ef440268bf299cbc
gatete <gatete@DESKTOP-OJJTRN4>
2022-10-05 11:02:58 +0200

    Kata 2 version 2

--- src/main/java/com/mycompany/kata2_v1_8_30/_30/Kata2_V1_8_3010_30.java
+++ src/main/java/com/mycompany/kata2_v1_8_30/_30/Kata2_V1_8_3010_30.java
@@ -11,12 +11,7 @@ public class Kata2_V1_8_3010_30 {
         Map <Integer, Integer> map = new HashMap<Integer, Integer>();
         
         for (int i = 0; i < data.length; i++){
-            
-            if (map.containsKey(data[i])){
-                map.put(data[i], map.get(data[i]) + 1);
-            } else {
-                map.put(data[i], 1);    
-            }
+            map.put(data[i], map.containsKey(data[i]) ? map.get(data[i]) + 1 : 1);
         }
         for (Map.Entry<Integer, Integer> entry: map.entrySet()){
             System.out.println(entry.getKey() + " ==> " + entry.getValue());
