7171f4a0335619c4f8f1cc347f5cc7c4501bb8f9
gatete <gatete@DESKTOP-OJJTRN4>
2022-10-05 11:08:39 +0200

    Kata 2 version 4

--- src/main/java/com/mycompany/kata2_v1_8_30/_30/histogramGenerator.java
+++ src/main/java/com/mycompany/kata2_v1_8_30/_30/histogramGenerator.java
@@ -3,22 +3,22 @@ package com.mycompany.kata2_v1_8_30._30;
 import java.util.HashMap;
 import java.util.Map;
 
-public class histogramGenerator {
-    private final int[] data;
-    public histogramGenerator(int[] data) {
+public class histogramGenerator<T> {
+    private final T[] data;
+    public histogramGenerator(T[] data) {
         this.data = data;
     }
 
-    public int[] getData() {
+    public T[] getData() {
         return data;
     }
     
-    public Map<Integer, Integer> getHistogram(){
-        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+    public Map<T, Integer> getHistogram(){
+        Map<T, Integer> histogram = new HashMap<T, Integer>();
             for (int i = 0; i < getData().length; i++) {
                 histogram.put(getData()[i], histogram.containsKey(getData()[i]) ? histogram.get(getData()[i]) + 1 : 1);
                 
             }
         return histogram;
-    }    
+    }
 }
